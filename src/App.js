afef641ec0b6cd950cfa912ad7ac5199500fa322
theCodingHQs <mrranjeet11115@gmail.com>
2025-10-13 21:08:43 +0530

    initialize react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form className='form'>
+
+        <div className='field-container item-container'>
+          <label>username</label>
+          <input />
+        </div>
+
+        <div className='field-container item-container'>
+          <label>password</label>
+          <input type='password' />
+        </div>
+
+        <div className='button-container item-container'>
+          <button type='button'>Submit</button>
+        </div>
+
+      </form>
     </div>
   );
 }
